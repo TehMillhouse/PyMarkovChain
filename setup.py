d87c82249a9050703d5ea5caaf4409fb73768076
Max Wagner <max@trollbu.de>
2013-03-31 05:59:45 +0200

    v1.6.1 Include README.md into PyPI package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyMarkovChain',
-    version='1.6',
+    version='1.6.1',
     author='Max Wagner',
     author_email='max@trollbu.de',
     packages=['pymarkovchain',],
